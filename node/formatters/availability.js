789d4c4109efc4abc9e3be452c9da95a8725730d
ryanskurkis <ryanskurkis@gmail.com>
2020-02-03 11:52:50 -0600

    fix comment

--- node/formatters/availability.js
+++ node/formatters/availability.js
@@ -4,7 +4,7 @@ const sortBy = require('lodash.sortby');
 /*
     I decided to format the availabilities into a flat array of objects.
     I did this for a few reasons.
-    1. It makes it easier for clients to group and sort how they wish.
+    1. It makes it easier for the server and client to operate on data like this
     2. formatting and returning objects allows us to grow the contract without breaking it.
     3. In a fully JS environment, formatting can be easily moved around during code review.
 */
